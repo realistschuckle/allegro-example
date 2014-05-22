0acb1d7d6f9772834f04b622c070a551000d6280
Curtis Schlak <realistschuckle@gmail.com>
2014-05-22 02:19:45 -0500

    Adding final files.

--- My Awesome Allegro Game/main.cpp
+++ My Awesome Allegro Game/main.cpp
@@ -1,14 +1,23 @@
-//
-//  main.m
-//  My Awesome Allegro Game
-//
-//  Created by Curtis Schlak on 5/22/14.
-//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
-//
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_color.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
 
-#import <Cocoa/Cocoa.h>
-
-int main(int argc, const char * argv[])
-{
-    return NSApplicationMain(argc, argv);
+int main(int argc, char **argv) {
+    al_init();
+    al_init_font_addon();
+    al_init_ttf_addon();
+    
+    ALLEGRO_DISPLAY* display = al_create_display(640, 480);
+    ALLEGRO_FONT* font = al_load_font("/Library/Fonts/Futura.ttc", 40, NULL);
+    
+    al_clear_to_color(al_color_html("#FF9900"));
+    al_draw_text(font, al_color_html("#FFFFFF"), 320, 200, ALLEGRO_ALIGN_CENTER, "LOOK AT THIS!");
+    al_flip_display();
+    al_rest(5.0);
+    
+    al_destroy_font(font);
+    al_destroy_display(display);
+    
+    return 0;
 }
